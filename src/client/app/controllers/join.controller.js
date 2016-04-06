deccd7d0c44cbe0bd79dc14e24073dec69e63147
Jin Park <jin@dramafever.com>
2016-04-06 15:24:48 -0400

    forgot name

--- src/client/app/controllers/join.controller.js
+++ src/client/app/controllers/join.controller.js
@@ -18,8 +18,10 @@
         console.log([response.player_guid, reponse.game_guid]);
         vm.user = response.player_guid;
         vm.game = response.game_guid;
+        vm.name = joinInfo.name;
         $window.localStorage.player_guid = response.player_guid;
         $window.localStorage.game_guid = response.game_guid;
+        $window.localStorage.name = joinInfo.name;
       };
 
       var failure = function(response) {
